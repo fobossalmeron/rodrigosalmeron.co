11b8875a428c3870cb509cdfe7e771a80521f47d
Rodrigo Salmeron <fobos.salmeron@gmail.com>
2021-01-21 23:10:00 -0600

    404

---
+++ src/routes/404.svelte
@@ -0,0 +1,57 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`locale/en.json`)
+      .then((r) => r.json())
+      .then((locale) => {
+        return { locale };
+      });
+  }
+</script>
+
+<script>
+  import TransitionWrapper from "../components/TransitionWrapper.svelte";
+  export let locale;
+</script>
+
+<style>
+  h1 {
+    display: inline;
+  }
+  p,
+  h1 {
+    font-size: 2.4rem;
+    max-width: 820px;
+    margin-top: 0;
+    width: 100%;
+  }
+  @media (max-width: 1000px) {
+    p,
+    h1 {
+      font-size: 1.8rem;
+      max-width: 640px;
+    }
+  }
+  @media (max-width: 700px) {
+    p,
+    h1 {
+      font-size: 1.4rem;
+    }
+  }
+  @media (max-width: 600px) {
+    p,
+    h1 {
+      font-size: 1.2rem;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>{locale.content.error.error_title}</title>
+</svelte:head>
+
+<TransitionWrapper>
+  <h1>{locale.content.error.title}</h1>
+  <p>
+    {@html locale.content.error.p}
+  </p>
+</TransitionWrapper>
--- src/routes/locale/_en.js
+++ src/routes/locale/_en.js
@@ -10,13 +10,10 @@ const en = {
       p:
         "Creative entrepreneur, interaction designer and developer based in CDMX. </br></br>Since 2008, I’ve worked with brand strategies, art direction and digital experiences in design agencies, film production companies and software development studios. I now specialize in solving business challenges through design-led digital technology.</br></br>I’m currently running <a href='https://acueducto.studio'>Acueducto</a>, an interface design and digital strategy firm, which I co-founded in late 2019.</br></br>Off-work, I’m a detail-driven music producer and lyricist, a steadily improving <a href='https://acueducto.studio/podcast'>technology podcast</a> host, an amateur tattooer and a sci-fi obsessed plant admirer. I studied a BA in visual communications, an MA in interaction design and taught myself how to code.</br></br>For detailed case studies of my work please visit <a href='https://acueducto.studio/en/work'>my firm's portfolio</a>.",
     },
-  },
-  error_page: {
-    headerTitle: "Error",
-    intro: {
-      title: "oops!<br/> page not found",
-      p:
-        "This page doesn’t exist, please check if the URL is correct. If you think this is a problem on our end please contact us at <a class='inline' href='mailto:soporte@acueducto.studio'>soporte@acueducto.studio</a>.",
+    error: {
+      error_title: "404– Page not found",
+      title: "404",
+      p: "Oops! Page not found",
     },
   },
 };
